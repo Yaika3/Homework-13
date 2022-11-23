3faf4ce2a6acfdeb811047f1e94436e724b56268
max <kabinidi@mail.ru>
2022-11-23 18:07:47 +0300

    1 ком

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String nameJack = "Jack";
+        String surnameLondon = "London";
+        String nameBookMartinEden = "Martin Eden";
+        int yearBookMartinEden = 1909;
+        String nameAgatha = "Agatha";
+        String surnameChristie = "Christie";
+        String nameBookMurderOnTheOrientExpress = "Murder on the orient express";
+        int yearBookMurderOnTheOrientExpress = 1933;
+
+
+
+    }
+}
